9e798e86a14c26954731cca439f37a6b8cdedaa2
cordea <cordear@outlook.com>
2021-06-15 23:21:13 +0800

    add new function about ADC2

--- Core/Src/main.c
+++ Core/Src/main.c
@@ -42,7 +42,9 @@
 
 /* Private variables ---------------------------------------------------------*/
 ADC_HandleTypeDef hadc1;
+ADC_HandleTypeDef hadc2;
 DMA_HandleTypeDef hdma_adc1;
+DMA_HandleTypeDef hdma_adc2;
 
 DAC_HandleTypeDef hdac1;
 
@@ -55,7 +57,10 @@ TIM_HandleTypeDef htim2;
 /* USER CODE BEGIN PV */
 __IO ITStatus UartReady = RESET;
 __IO uint16_t adcValues[DMABUFFER];
+__IO uint16_t staticAdcValues[DMABUFFER];
 uint16_t aTxBuffer[1];
+
+__IO uint16_t maxValue = 0;
 /* USER CODE END PV */
 
 /* Private function prototypes -----------------------------------------------*/
@@ -67,8 +72,10 @@ static void MX_ADC1_Init(void);
 static void MX_TIM2_Init(void);
 static void MX_DAC1_Init(void);
 static void MX_SPI1_Init(void);
+static void MX_ADC2_Init(void);
 /* USER CODE BEGIN PFP */
-
+static void FindMaxValue(void);
+static void Max7219Show(uint16_t number);
 /* USER CODE END PFP */
 
 /* Private user code ---------------------------------------------------------*/
@@ -81,7 +88,7 @@ static void MX_SPI1_Init(void);
 
 PUTCHAR_PROTOTYPE
 {
-  //具体哪个串口可以更改huart1为其它串�???????????????????
+  //具体哪个串口可以更改huart1为其它串�????????????????????
   HAL_UART_Transmit(&hlpuart1, (uint8_t *)&ch, 1, 0xffff);
   return ch;
 }
@@ -122,6 +129,7 @@ int main(void)
   MX_TIM2_Init();
   MX_DAC1_Init();
   MX_SPI1_Init();
+  MX_ADC2_Init();
   /* USER CODE BEGIN 2 */
   if (HAL_TIM_Base_Start_IT(&htim2) != HAL_OK)
   {
@@ -131,16 +139,23 @@ int main(void)
   {
     Error_Handler();
   }
+  if (HAL_ADCEx_Calibration_Start(&hadc2, ADC_SINGLE_ENDED) != HAL_OK)
+  {
+    Error_Handler();
+  }
   // printf("%s\n", "ADC calibration complete.");
   if (HAL_ADC_Start_DMA(&hadc1, (uint32_t *)adcValues, DMABUFFER) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  if (HAL_ADC_Start_DMA(&hadc2, (uint32_t *)staticAdcValues, DMABUFFER) != HAL_OK)
   {
     Error_Handler();
   }
   max7219_Init(7);
   max7219_Decode_On();
   max7219_Clean();
-  max7219_PrintDigit(DIGIT_1, 1, true);
-  max7219_PrintDigit(DIGIT_2, 2, false);
+  Max7219Show(0);
   /* USER CODE END 2 */
 
   /* Infinite loop */
@@ -285,6 +300,61 @@ static void MX_ADC1_Init(void)
   /* USER CODE BEGIN ADC1_Init 2 */
 
   /* USER CODE END ADC1_Init 2 */
+}
+
+/**
+  * @brief ADC2 Initialization Function
+  * @param None
+  * @retval None
+  */
+static void MX_ADC2_Init(void)
+{
+
+  /* USER CODE BEGIN ADC2_Init 0 */
+
+  /* USER CODE END ADC2_Init 0 */
+
+  ADC_ChannelConfTypeDef sConfig = {0};
+
+  /* USER CODE BEGIN ADC2_Init 1 */
+
+  /* USER CODE END ADC2_Init 1 */
+  /** Common config
+  */
+  hadc2.Instance = ADC2;
+  hadc2.Init.ClockPrescaler = ADC_CLOCK_ASYNC_DIV1;
+  hadc2.Init.Resolution = ADC_RESOLUTION_12B;
+  hadc2.Init.DataAlign = ADC_DATAALIGN_RIGHT;
+  hadc2.Init.ScanConvMode = ADC_SCAN_DISABLE;
+  hadc2.Init.EOCSelection = ADC_EOC_SINGLE_CONV;
+  hadc2.Init.LowPowerAutoWait = DISABLE;
+  hadc2.Init.ContinuousConvMode = DISABLE;
+  hadc2.Init.NbrOfConversion = 1;
+  hadc2.Init.DiscontinuousConvMode = DISABLE;
+  hadc2.Init.ExternalTrigConv = ADC_EXTERNALTRIG_T2_TRGO;
+  hadc2.Init.ExternalTrigConvEdge = ADC_EXTERNALTRIGCONVEDGE_RISING;
+  hadc2.Init.DMAContinuousRequests = DISABLE;
+  hadc2.Init.Overrun = ADC_OVR_DATA_PRESERVED;
+  hadc2.Init.OversamplingMode = DISABLE;
+  if (HAL_ADC_Init(&hadc2) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /** Configure Regular Channel
+  */
+  sConfig.Channel = ADC_CHANNEL_8;
+  sConfig.Rank = ADC_REGULAR_RANK_1;
+  sConfig.SamplingTime = ADC_SAMPLETIME_2CYCLES_5;
+  sConfig.SingleDiff = ADC_SINGLE_ENDED;
+  sConfig.OffsetNumber = ADC_OFFSET_NONE;
+  sConfig.Offset = 0;
+  if (HAL_ADC_ConfigChannel(&hadc2, &sConfig) != HAL_OK)
+  {
+    Error_Handler();
+  }
+  /* USER CODE BEGIN ADC2_Init 2 */
+
+  /* USER CODE END ADC2_Init 2 */
 }
 
 /**
@@ -464,6 +534,9 @@ static void MX_DMA_Init(void)
   /* DMA1_Channel1_IRQn interrupt configuration */
   HAL_NVIC_SetPriority(DMA1_Channel1_IRQn, 0, 0);
   HAL_NVIC_EnableIRQ(DMA1_Channel1_IRQn);
+  /* DMA1_Channel2_IRQn interrupt configuration */
+  HAL_NVIC_SetPriority(DMA1_Channel2_IRQn, 0, 0);
+  HAL_NVIC_EnableIRQ(DMA1_Channel2_IRQn);
 }
 
 /**
@@ -509,15 +582,22 @@ void HAL_UART_TxCpltCallback(UART_HandleTypeDef *UartHandle)
 void HAL_ADC_ConvCpltCallback(ADC_HandleTypeDef *AdcHandle)
 {
   uint16_t i = 0;
-  if (AdcHandle->Instance == hadc1.Instance)
+  if (AdcHandle->Instance == hadc2.Instance)
   {
     // printf("%s\n", "****Begin****");
     for (i = 0; i < DMABUFFER; ++i)
     {
-      printf("%d\n", adcValues[i]);
+      printf("%d\n", staticAdcValues[i]);
     }
     i = 0;
+    FindMaxValue();
+    Max7219Show(maxValue);
     // printf("%s\n", "****End****");
+  }
+  if (AdcHandle->Instance == hadc2.Instance)
+  {
+    printf("%s\n", "ADC2over");
+    printf("%d\n", staticAdcValues[2000]);
   }
   // printf("%s\n", "Processing");
 #ifndef ONESHOT
@@ -534,6 +614,31 @@ void HAL_SPI_TxCpltCallback(SPI_HandleTypeDef *hspi)
   {
     printf("%s", "SPI Send Finish\n");
   }
+}
+
+static void FindMaxValue(void)
+{
+  maxValue = adcValues[0];
+  uint16_t i;
+  for (i = 600; i < DMABUFFER; ++i)
+  {
+    if (adcValues[i] > maxValue)
+    {
+      maxValue = adcValues[i];
+    }
+  }
+}
+static void Max7219Show(uint16_t number)
+{
+  uint16_t a = number / 1000;
+  uint16_t b = (number % 1000) / 100;
+  uint16_t c = (number % 100) / 10;
+  uint16_t d = number % 10;
+  max7219_Clean();
+  max7219_PrintDigit(DIGIT_4, a, false);
+  max7219_PrintDigit(DIGIT_3, b, false);
+  max7219_PrintDigit(DIGIT_2, c, false);
+  max7219_PrintDigit(DIGIT_1, d, false);
 }
 /* USER CODE END 4 */
 
